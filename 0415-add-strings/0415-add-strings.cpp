1a173e8ee448cb98e296ddf4d850040eb509a16a
Aakarshit upveja <aakarshitupveja@gmail.com>
2026-08-14 02:04:55 +0530

    Time: 0 ms (100%), Space: 9 MB (79.47%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+
+        int i = num1.size() - 1;
+        int j = num2.size() - 1;
+        int carry = 0;
+        string result;
+        
+        while (i >= 0 || j >= 0 || carry) {
+            int d1 = (i >= 0) ? num1[i] - '0' : 0;
+            int d2 = (j >= 0) ? num2[j] - '0' : 0;
+            int total = d1 + d2 + carry;
+            carry = total / 10;
+            result.push_back('0' + total % 10);
+            i--;
+            j--;
+        }
+        
+        reverse(result.begin(), result.end());
+        return result;
+
+        
+    }
+};
