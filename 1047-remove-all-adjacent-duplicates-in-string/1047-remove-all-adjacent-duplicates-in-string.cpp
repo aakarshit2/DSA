0786bae8f219a3598c62cd88dc5350b47dc1e0b6
Aakarshit upveja <aakarshitupveja@gmail.com>
2026-08-18 11:18:09 +0530

    Time: 7 ms (50.76%), Space: 13.6 MB (74.18%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string a;
+    for(int i =0;i<s.length();i++){
+        if(!a.empty() && a.back()==s[i]){
+            a.pop_back();
+
+        }
+        else{
+            a.push_back(s[i]);
+        }
+    }
+    return a;
+    }
+};
