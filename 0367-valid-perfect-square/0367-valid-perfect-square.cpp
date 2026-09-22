1b3dbeeebbedf628b913a5a239669059ece6ed98
Aakarshit upveja <aakarshitupveja@gmail.com>
2026-09-22 11:45:58 +0530

    Time: 0 ms (100%), Space: 7.7 MB (88.04%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        long low=0;
+        long high=num;
+        while(low<=high){
+            long mid=low+(high-low)/2;
+            long sq=mid*mid;
+            if(sq==num){
+                return true;
+            }
+            else if(sq<num){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return false;
+    }
+};
